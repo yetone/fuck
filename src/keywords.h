857a58df4a593e6c1c8ba85c008d06c83e513ca9
redpois0n <red@cmail.nu>
2015-07-14 18:38:38 +0200

    defines

--- src/keywords.h
+++ src/keywords.h
@@ -2,8 +2,18 @@
 
 using namespace std;
 
+#define KW_END "end_"
+
 #define KW_FALSE "false"
 #define KW_TRUE "true"
+
+#define KW_NAMESPACE "namespace"
+#define KW_END_NAMESPACE KW_END + KW_NAMESPACE
+
+#define KW_METHOD "method"
+#define KW_END_METHOD KW_END + KW_METHOD
+
+#define KW_PRINT "print"
 
 void load();
 
