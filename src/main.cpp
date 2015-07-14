b93fe8cde86c464d725c7d6d4a49710970bde9fd
redpois0n <red@cmail.nu>
2015-07-14 17:05:56 +0200

    Parser begin, parses each argument (not first)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+#include "keywords.h"
+#include "parser.h"
+
 using namespace std;
 
-int main(void) {
+int main(int argc, char* argv[]) {
+
+	if (argc <= 1) {
+		cerr << "No input files specified";
+		return 1;
+	}
+
+	// Skip first in argument list
+	for (int i = 1; i < argc; i++) {
+		parse(argv[i]);
+	}
+
 	return 0;
 }
---
+++ src/parser.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+
+using namespace std;
+
+void parse(char* file) {
+	ifstream input(file);
+
+	string line;
+	while (getline(input, line)) {
+
+	}
+}
