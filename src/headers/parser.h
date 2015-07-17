aeb3929529633918ec11f094b418ac3da12b96c4
redpois0n <red@cmail.nu>
2015-07-17 14:23:43 +0200

    Some comments

--- src/headers/parser.h
+++ src/headers/parser.h
@@ -8,22 +8,30 @@ using namespace std;
 
 typedef vector<Method*> methodmap;
 
+// Check if line is comment
 inline bool is_comment(string);
 
+// Check if line is goto label
 inline bool is_label(string);
 
 void parse(Code*);
 
+// Invoke main method
 void invoke();
 
+// Invoke method from string
 void invoke(string);
 
+// Invoke method
 void invoke(Method*);
 
+// Execute line
 void execline(Method*, unsigned int*);
 
+// Execute range of lines
 void execrange(Method*, unsigned int*, unsigned int);
 
+// check if something is true
 bool check_cond(string);
 
 struct If {
