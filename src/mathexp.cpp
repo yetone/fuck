d96be5309ecb3774863708c62a22ca9da0fd664a
redpois0n <red@cmail.nu>
2015-07-23 14:05:05 +0200

    mathexp file

---
+++ src/mathexp.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+#include "headers/utils.h"
+#include "headers/colors.h"
+
+using namespace std;
+
+int eval(string s) {
+	printverbose("Evaluating mathematical expression " + color(VERBOSE_HL) + s);
+	return 0;
+}
