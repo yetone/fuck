b7a753281a3c1887bf5f333acc36ef87e14ccd64
redpois0n <red@cmail.nu>
2015-08-02 15:57:26 +0200

    main.h

---
+++ src/headers/main.h
@@ -0,0 +1,14 @@
+#define NORMAL_EXIT 0
+#define VERSION "0.0"
+
+#define ARG_VERBOSE "--verbose"
+#define ARG_VERBOSE_SHORT "-v"
+#define ARG_COLORS "--colors"
+#define ARG_COLORS_SHORT "-c"
+#define ARG_HELP "--help"
+
+#define EXIT L"exit"
+
+void printinfo();
+
+void printusage();
--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,9 @@
 #include "headers/code.h"
 #include "headers/engine.h"
 #include "headers/var.h"
+#include "headers/main.h"
 
 using namespace std;
-
-#define NORMAL_EXIT 0
-#define VERSION "0.0"
-
-#define ARG_VERBOSE "--verbose"
-#define ARG_VERBOSE_SHORT "-v"
-#define ARG_COLORS "--colors"
-#define ARG_COLORS_SHORT "-c"
-#define ARG_HELP "--help"
-
-#define EXIT L"exit"
 
 bool verbose = false;
 bool colors = false;
